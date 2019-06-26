4fe70b4eed5068dcb60b36caffba49b61d0c25e2
Simon Guest <simon.guest@tesujimath.org>
2019-06-26 16:10:58 +1200

    Add vcl-mode

--- init-annex.el
+++ init-annex.el
@@ -23,6 +23,8 @@
                                             (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
                                             (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
                                             ))
+(require-package 'vcl-mode)
+
 ;;
 ;; flyspell in some additional modes
 ;;
