6e477e8e8bf66c18a7135062c77606b88bda49af
Simon Guest <sguest@educationperfect.com>
2022-07-11 14:34:34 +1200

    deadgrep: hidden and follow
    Stolen from https://github.com/Wilfred/deadgrep/issues/24#issuecomment-942290197

--- init-annex-deadgrep.el
+++ init-annex-deadgrep.el
@@ -6,5 +6,13 @@
 
 (require-package 'deadgrep)
 
+(after-load 'deadgrep
+  (defun deadgrep--include-args (rg-args)
+    (push "--hidden" rg-args) ;; consider hidden folders/files
+    (push "--follow" rg-args) ;; follow symlink
+    )
+  (advice-add 'deadgrep--arguments :filter-return #'deadgrep--include-args)
+  )
+
 (provide 'init-annex-deadgrep)
 ;;; init-annex-deadgrep.el ends here
