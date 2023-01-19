fdfafd2316db85fee53f50162ff2c7c5129c98c4
Simon Guest <simon.guest@tesujimath.org>
2023-01-20 11:17:34 +1300

    Don't make deadgrep search in hidden files or follow symlinks
    Needs to be switchable.

--- init-annex-deadgrep.el
+++ init-annex-deadgrep.el
@@ -6,13 +6,16 @@
 
 (require-package 'deadgrep)
 
-(after-load 'deadgrep
-  (defun deadgrep--include-args (rg-args)
-    (push "--hidden" rg-args) ;; consider hidden folders/files
-    (push "--follow" rg-args) ;; follow symlink
-    )
-  (advice-add 'deadgrep--arguments :filter-return #'deadgrep--include-args)
-  )
+;; TODO this should be switchable
+;; (after-load 'deadgrep
+;;   (defun deadgrep--include-args (rg-args)
+;;     (push "--hidden" rg-args) ;; consider hidden folders/files
+;;     (push "--follow" rg-args) ;; follow symlink
+;;     )
+;; (advice-add 'deadgrep--arguments :filter-return #'deadgrep--include-args)
+;;   )
+
+;; (advice-remove 'deadgrep--arguments 'deadgrep--include-args)
 
 (provide 'init-annex-deadgrep)
 ;;; init-annex-deadgrep.el ends here
