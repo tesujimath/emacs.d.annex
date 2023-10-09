5a3dc5ea250c70498c6f372709c8632df500d273
Simon Guest <simon.guest@tesujimath.org>
2023-10-09 14:53:57 +1300

    Remove org-roam
    Incompatible with orgzly, so I won't be using it

--- init-local.el
+++ init-local.el
@@ -51,7 +51,6 @@
 (require 'init-annex-nix)
 (require 'init-annex-org)
 (require 'init-annex-org-jira)
-(require 'init-annex-org-roam)
 (require 'init-annex-powershell)
 (require 'init-annex-r)
 (require 'init-annex-rust)
