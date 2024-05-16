083e0206591fba833920bf8b60c763f9f7d3404f
Simon Guest <simon.guest@agresearch.co.nz>
2024-05-17 10:45:05 +1200

    Nushell tesujimath/clean-buffer only removes right prompt now

--- init-annex-nushell.el
+++ init-annex-nushell.el
@@ -7,31 +7,8 @@
 (require-package 'nushell-mode)
 
 (defun tesujimath/clean-buffer () (interactive)
-       "clean nushell and zellij markup from current buffer"
+       "clean nushell markup from current buffer"
        (save-excursion
-         ;; indent any nushell tables by a couple of spaces to protect them from zellij frame removal
-         (beginning-of-buffer)
-         (while (re-search-forward "^│\\([╭│├╰]\\)" nil t)
-           (replace-match "│  \\1"))
-
-         ;; kill the blank lines
-         (beginning-of-buffer)
-         (while (re-search-forward "^│\\s *│$" nil t)
-           (replace-match ""))
-
-         ;; kill the lines with content
-         (beginning-of-buffer)
-         (while (re-search-forward "^│\\(.*[^ ]\\) *│$" nil t)
-           (replace-match "\\1"))
-
-         ;; kill any stray bars at the beginning of a line
-         ;; which sometimes arise because the line end is truncated
-         ;; (may be a bug in alacritty or zellij)
-         ;; but this destroys nushell tables
-         (beginning-of-buffer)
-         (while (re-search-forward "^│" nil t)
-           (replace-match ""))
-
          ;; remove nushell right prompt
          (beginning-of-buffer)
          (while (re-search-forward " *\\([0-9]+ \\)?[0-9]\\{1,2\\}/[0-9]\\{1,2\\}/[0-9]\\{2,4\\} [0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\} [AP]M$" nil t)
